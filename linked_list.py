03e7ca3a964afe1f8b7d5c5166dbd866510e9e18
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-10-30 12:51:33 +0300

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,95 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        out = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                out.append(node)
+            node = node.next
+
+        return out
+
+    def delete(self, val, all=False):
+        if self.head is None:
+            return
+        node = self.head
+        prev = self.head
+        while node is not None:
+            if prev.next is node:
+                prev = node
+            node = node.next
+            if self.head.value == val:
+                self.head = prev.next
+                if not all:
+                    break
+
+            elif node is self.tail and self.tail.value == val:
+                prev.next = None
+                self.tail = prev
+                if not all:
+                    break
+
+            elif node is not None and node.value == val:
+                prev.next = node.next
+                if not all:
+                    break
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+
+    def len(self):
+        length = 0
+        node = self.head
+        while node is not None:
+            length += 1
+            node = node.next
+        return length
+
+    def insert(self, afterNode, newNode):
+        node = self.head
+        if afterNode is None:
+            newNode.next = node.next
+            self.head = newNode
+        else:
+            while node is not None:
+                if node is afterNode:
+                    newNode.next = node.next
+                    node.next = newNode
+                if afterNode == self.tail:
+                    self.tail = newNode
+                    break
+                node = node.next
